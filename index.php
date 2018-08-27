fda3533ca2809f41e54536466348492197130e78
Romara <macroromara@gmail.com>
2018-08-27 12:44:36 +0300

    Adding new file

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet"  href="css/bootstrap.min.css"/>
+    <script src="main.js"></script>
+</head>
+<body>
+<?php  
+     echo"<div class='head'>
+       <h1> RECRUITED STUDENT TO JOIN IN DIT </h1>
+     </div>";
+     
+    
+?>
+    <div>
+       <form action="">
+         <input type="text" name="username" id="" placeholder="Enter user name">
+         <input type="email" name="email" id="" placeholder="Enter Email">
+         <input type=password name="password">
+       </form>
+     </div>
+     <div>
+       <form action="">
+         <input type="text" name="username" id="" placeholder="Enter user name">
+         <input type="email" name="email" id="" placeholder="Enter Email">
+         <input type=password name="password">
+       </form>
+     </div>
+     <div>
+       <form action="">
+         <input type="text" name="username" id="" placeholder="Enter user name">
+         <input type="email" name="email" id="" placeholder="Enter Email">
+         <input type=password name="password">
+       </form>
+     </div>
+     <div>
+       <form action="">
+         <input type="text" name="username" id="" placeholder="Enter user name">
+         <input type="email" name="email" id="" placeholder="Enter Email">
+         <input type=password name="password">
+       </form>
+     </div>
+</body>
+</html>
